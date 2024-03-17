ce5a294c943e25899778c4a8b4efda1f86f01f10
Adarsh <108751847+zs0c131y@users.noreply.github.com>
2024-03-17 15:53:37 +0530

    Add files via upload
    Aligned "edit profile" and date view in notifications.

--- notification.js
+++ notification.js
@@ -127,7 +127,7 @@ var data = [
           display: inline;
           font-size:14px;
           color:grey;
-          justify-content:flex-end;
+          margin-left: 790px;
         
       `;
     authorContainer1.appendChild(authorName);
